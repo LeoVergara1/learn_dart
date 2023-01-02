cf4c07dbef0724e1562d4f460bb4fc0f2998f270
Brandon Vergara <brandon@makingdevs.com>
2023-01-02 16:47:04 -0600

    [main] add more examples

--- Future.dart
+++ Future.dart
@@ -1,30 +1,3 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Codespaces
-Marketplace
-Explore
-
-@LeoVergara1
-Klerith
-/
-dart-basics
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-dart-basics/13-futures.dart
-@Klerith
-Klerith Update 13-futures.dart
-Latest commit c793ac3 on Jun 5, 2021
- History
- 1 contributor
-30 lines (13 sloc)  361 Bytes
 
 void main() {
 
@@ -50,23 +23,3 @@ Future<String> httpGet( String url ) {
     Duration( seconds: 3 ), () =>'Hola Mundo - 3 segundos'
   );
 }
-
-
-
-
-
-
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
